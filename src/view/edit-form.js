b756c26675386d3de29d0be2e75b54a1dd09bc29
Barbalob <a79221932005@gmail.com>
2023-06-06 16:38:52 +0500

    Обновление века (часть 1 Закончено)

--- src/view/edit-form.js
+++ src/view/edit-form.js
@@ -79,7 +79,7 @@ const createFiltersTemplate = (point, destination, offersByType) => {
   </header>
 
   <section class="event__details">
-  ${typeOffers.lenght ? `
+  ${typeOffers.length ? `
   <section class="event__section  event__section--offers">
   <h3 class="event__section-title  event__section-title--offers">Offers</h3>
 
