bdfb5af91100d57ce93f685ef4cbb9daa2e45a8c
Barbalob <a79221932005@gmail.com>
2023-06-19 19:05:15 +0500

    Правки-5

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 // import ListFilterView from './view/filters.js';
-import ListHeadersView from './view/headers.js';
+// import ListHeadersView from './view/headers.js';
 import ListMenuView from './view/menu.js';
 import Trip from './presenter/trip.js';
 import PointModel from './model/point-model.js';
 import FilterPresenter from './presenter/filter-presenter.js';
 import FilterModel from './model/filter-model.js';
 import NewPointButton from './view/new-point-button.js';
-import { RenderPosition, render } from './framework/render.js';
+import { render } from './framework/render.js';
 import PoitApiService from './point-api-service.js';
+import MenuPresenter from './presenter/menu-presenter.js';
 
 const AUTHORIZATION = 'Basic basi_k2qqdvs3vzvw22t_basic';
 const END_POINT = 'https://18.ecmascript.pages.academy/big-trip';
@@ -16,10 +17,7 @@ const filterContainer = document.querySelector('.trip-controls__filters');
 const tripMainContainer = document.querySelector('.trip-main');
 const tripContainer = document.querySelector('.trip-events');
 const menuContainer = document.querySelector('.trip-controls__navigation');
-// const siteMain = document.querySelector('.page-main');
 
-// render(new ListFilterView(), filterContainer);
-render(new ListHeadersView(), tripMainContainer, RenderPosition.AFTERBEGIN);
 render(new ListMenuView(), menuContainer);
 
 const model = new PointModel({pointsApiService: new PoitApiService(END_POINT, AUTHORIZATION)});
@@ -33,6 +31,9 @@ tripPresenter.init();
 
 const filterPresenter = new FilterPresenter({container:filterContainer, pointModel: model, filterModel: filterModel});
 filterPresenter.init();
+
+const menuPresenter = new MenuPresenter({container:tripMainContainer, pointModel: model});
+menuPresenter.init();
 
 
 newPointButtonComponent.setClickHandler(handleNewPointButtonClick);
